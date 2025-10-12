6d52a7a484d78909d8f3f9bc2a09c8f636136a50
mukesh <mukeshsuthar@192.168.0.111>
2025-10-13 00:00:18 +0530

    profile info section created of profile page

---
+++ frontend/src/components/UserHeader.jsx
@@ -0,0 +1,94 @@
+import {
+  Avatar,
+  Box,
+  Flex,
+  Link,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
+  Portal,
+  Text,
+  useToast,
+  VStack,
+} from "@chakra-ui/react";
+import { BsInstagram } from "react-icons/bs";
+import { CgMoreO } from "react-icons/cg";
+
+const UserHeader = () => {
+  const toast = useToast();
+
+  const copyURL = () => {
+    const currentURL = window.location.href;
+    navigator.clipboard.writeText(currentURL).then(() => {
+      toast({
+        title: "Account created.",
+        status: "success",
+        description: "Copied!",
+        duration: 3000,
+        isClosable: true,
+      });
+    });
+  };
+
+  return (
+    <VStack gap={4} alignItems={"start"}>
+      <Flex justifyContent={"space-between"} w={"full"}>
+        <Box>
+          <Text fontSize={"2xl"} fontWeight={"bold"}>
+            Mark Zuckerberg
+          </Text>
+          <Flex gap={2} alignItems={"center"}>
+            <Text fontSize={"sm"}>markzuckerberg</Text>
+            <Text
+              fontSize={"xs"}
+              bg={"gray.dark"}
+              color={"gray.light"}
+              p={1}
+              borderRadius={"full"}
+            >
+              threads.net
+            </Text>
+          </Flex>
+        </Box>
+        <Box>
+          <Avatar
+            name="Mark Zuckerberg"
+            src="/zuck-avatar.png"
+            size={"xl"}
+          ></Avatar>
+        </Box>
+      </Flex>
+
+      <Text>Co-founder, executive chairman and CEO of Meta Platform.</Text>
+      <Flex w={"full"} justifyContent={"space-between"}>
+        <Flex gap={2} alignItems={"center"}>
+          <Text color={"gray.light"}>3.4K followers</Text>
+          <Box w={1} h={1} bg={"gray.light"} borderRadius={"full"}></Box>
+          <Link color={"gray.light"}>instagram.com</Link>
+        </Flex>
+        <Flex>
+          <Box className="icon-container">
+            <BsInstagram size={24} cursor={"pointer"} />
+          </Box>
+          <Box className="icon-container">
+            <Menu>
+              <MenuButton>
+                <CgMoreO size={24} cursor={"pointer"} />
+              </MenuButton>
+              <Portal>
+                <MenuList bg={"gray.dark"}>
+                  <MenuItem bg={"gray.dark"} onClick={copyURL}>
+                    Copy Link
+                  </MenuItem>
+                </MenuList>
+              </Portal>
+            </Menu>
+          </Box>
+        </Flex>
+      </Flex>
+    </VStack>
+  );
+};
+
+export default UserHeader;
