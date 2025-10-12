6b4ac35a190267ad4ae67c5f930534837740cd34
mukesh <mukeshsuthar@192.168.0.111>
2025-10-12 17:58:14 +0530

    Initial commit

---
+++ frontend/src/main.jsx
@@ -0,0 +1,41 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import { ChakraProvider, ColorModeScript, extendTheme } from '@chakra-ui/react'
+import { mode } from '@chakra-ui/theme-tools'
+import { BrowserRouter } from 'react-router-dom'
+
+const styles = {
+  global:(props) => ({
+    body: {
+      color: mode('gray.800', 'whiteAlpha.900')(props),
+      bg: mode('gray.100', '#101010')(props),
+    }
+  })
+}
+
+const config = {
+  initialColorMode: 'dark',
+  useSystemColorMode: true,
+}
+
+const color = {
+  gray: {
+    light: '#616161',
+    dark: '#1e1e1e'
+  }
+}
+
+const theme = extendTheme({ styles, config, color });
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <BrowserRouter>
+      <ChakraProvider theme={theme}>
+        <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+        <App />
+      </ChakraProvider>
+    </BrowserRouter>
+  </StrictMode>,
+)
