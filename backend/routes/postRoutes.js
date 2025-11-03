35180eebf91e76290ed44196cd6aa3b0e3743e93
mukesh <sutharmukesh910@gmail.com>
2025-11-03 23:20:25 +0530

    create post functionality implemented

--- backend/routes/postRoutes.js
+++ backend/routes/postRoutes.js
@@ -13,9 +13,9 @@ import protectRoute from "../middlewares/protectRoute.js";
 const router = express.Router();
 
 router.get("/feed", protectRoute, getFeedPosts);
-router.get("/:id", getPost);
 router.get("/user/:username", getUserPosts);
 router.post("/create", protectRoute, createPost);
+router.get("/:id", getPost);
 router.delete("/:id", protectRoute, deletePost);
 router.put("/like/:id", protectRoute, likeUnlikePost);
 router.put("/reply/:id", protectRoute, replyToPost);
--- frontend/src/components/LoginCard.jsx
+++ frontend/src/components/LoginCard.jsx
@@ -24,6 +24,7 @@ export default function LoginCard() {
   const [showPassword, setShowPassword] = useState(false);
   const setAuthScreen = useSetRecoilState(authScreenAtom);
   const setUser = useSetRecoilState(userAtom);
+  const [loading, setLoading] = useState(false);
 
   const [inputs, setInputs] = useState({
     username: "",
@@ -31,6 +32,7 @@ export default function LoginCard() {
   });
   const showToast = useShowToast();
   const handleLogin = async () => {
+    setLoading(true);
     try {
       const res = await fetch("/api/users/login", {
         method: "POST",
@@ -49,6 +51,8 @@ export default function LoginCard() {
       setUser(data);
     } catch (error) {
       showToast("Error", error, "error");
+    } finally {
+      setLoading(false);
     }
   };
   return (
@@ -99,9 +103,7 @@ export default function LoginCard() {
                 <InputRightElement h={"full"}>
                   <Button
                     variant={"ghost"}
-                    onClick={() =>
-                      setShowPassword((showPassword) => !showPassword)
-                    }
+                    onClick={() => setShowPassword((showPassword) => !showPassword)}
                   >
                     {showPassword ? <ViewIcon /> : <ViewOffIcon />}
                   </Button>
@@ -118,6 +120,7 @@ export default function LoginCard() {
                   bg: useColorModeValue("gray.700", "gray.800"),
                 }}
                 onClick={handleLogin}
+                isLoading={loading}
               >
                 Login
               </Button>
@@ -125,10 +128,7 @@ export default function LoginCard() {
             <Stack pt={6}>
               <Text align={"center"}>
                 Don&apos;t have an account?{" "}
-                <Link
-                  color={"blue.400"}
-                  onClick={() => setAuthScreen("signup")}
-                >
+                <Link color={"blue.400"} onClick={() => setAuthScreen("signup")}>
                   Sign up
                 </Link>
               </Text>
