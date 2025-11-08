8efaa8e7f3010f669cfbf36e3964e55f9e30b841
mukesh <sutharmukesh910@gmail.com>
2025-11-08 20:28:11 +0530

    Post page added and useGetUserProfile custom hook created

--- frontend/src/pages/UserPage.jsx
+++ frontend/src/pages/UserPage.jsx
@@ -4,40 +4,22 @@ import { useParams } from "react-router-dom";
 import useShowToast from "../hooks/useShowToast";
 import { Flex, Spinner } from "@chakra-ui/react";
 import Post from "../components/Post";
+import useGetUserProfile from "../hooks/useGetUserProfile";
 
 const UserPage = () => {
-  const [user, setUser] = useState(null);
+  const { user, loading } = useGetUserProfile();
   const { username } = useParams();
   const showToast = useShowToast();
-  const [loading, setLoading] = useState(true);
   const [posts, setPosts] = useState([]);
   const [fetchingPosts, setFetchingPosts] = useState(true);
 
   useEffect(() => {
-    const getUser = async () => {
-      try {
-        const res = await fetch(`/api/users/profile/${username}`);
-        const data = await res.json();
-        if (data.error) {
-          showToast("Error", data.error, "error");
-          return;
-        }
-        setUser(data);
-      } catch (error) {
-        showToast("Error", error, "error");
-      } finally {
-        setLoading(false);
-      }
-    };
-
     const getPosts = async () => {
       setFetchingPosts(true);
       try {
         const res = await fetch(`/api/posts/user/${username}`);
         const data = await res.json();
-        console.log("data====>", data);
         setPosts(data);
-        console.log("posts======>", posts)
       } catch (error) {
         showToast("Error", error.message, "error");
         setPosts([]);
@@ -46,7 +28,6 @@ const UserPage = () => {
       }
     };
 
-    getUser();
     getPosts();
   }, [username, showToast]);
 
